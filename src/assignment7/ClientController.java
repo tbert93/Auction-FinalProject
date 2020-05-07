37b0dc04c0d686a47b1de4ce3dbfd5db48ce5ccf
Tony Bertolino <tony.bertolino@utexas.edu>
2020-05-07 12:34:23 -0500

    Bidding function now works.

--- src/assignment7/ClientController.java
+++ src/assignment7/ClientController.java
@@ -1,37 +1,76 @@
 package assignment7;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.io.*;
 import java.net.*;
-import java.nio.Buffer;
 import java.util.Scanner;
 
 public class ClientController {
 
     public final static String DATABASE = "src/assignment7/database.txt";
+    public final static String JUST_ITEMS = "src/assignment7/justItems.txt";
     public final static int PORT = 4242;
     public final static String HOST = "localhost";
     public final static int FILE_SIZE = 6022386;
+    public final static String USERNAME = "Client 1";
+    public final static String PASSWORD = "1234";
 
-    public TextArea itemList;
+    @FXML
+    private TextArea itemList;
+    @FXML
+    private Button bidButton;
+    @FXML
+    private ChoiceBox<String> dropDown;
+    @FXML
+    private Slider slider;
+    @FXML
+    private Label sliderLabel;
 
     //initializes upon startup
     @FXML
     void initialize() throws IOException {
         //download database from server
-        getDatabase();
+        getFile(DATABASE);
+        //download justItems from server
+        getFile(JUST_ITEMS);
         //output database to GUI
         updateAuctionList();
-
-
-
-
+        //update dropdown menu with items
+        updateDropDown();
 
     }
 
-    public static void getDatabase()throws IOException{
+    @FXML
+    void bid(MouseEvent event) {
+        try{
+            //establish connection with server
+            Socket socket = new Socket(HOST,PORT);
+            //create an output stream to server
+            ObjectOutputStream toServer = new ObjectOutputStream(socket.getOutputStream());
+
+            Double value = slider.getValue();
+            String item = dropDown.getValue();
+            String description = findDescription(item);
+
+            //create an Item object and send to the server
+            Item i = new Item(item,description,value,USERNAME);
+            toServer.writeObject(i);
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    void showMoney(MouseEvent event){
+        int sliderValue = (int) slider.getValue();
+        sliderLabel.setText(Integer.toString(sliderValue));
+    }
+
+    public static void getFile(String file)throws IOException{
         int bytesRead;
         int current = 0;
         FileOutputStream fileOutputStream = null;
@@ -44,7 +83,7 @@ public class ClientController {
             // receive file
             byte [] bytes  = new byte [FILE_SIZE];
             InputStream is = socket.getInputStream();
-            fileOutputStream = new FileOutputStream(DATABASE);
+            fileOutputStream = new FileOutputStream(file);
             bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
             bytesRead = is.read(bytes,0,bytes.length);
             current = bytesRead;
@@ -57,7 +96,7 @@ public class ClientController {
 
             bufferedOutputStream.write(bytes, 0 , current);
             bufferedOutputStream.flush();
-            System.out.println("File " + DATABASE
+            System.out.println("File " + file
                     + " downloaded (" + current + " bytes read)");
         }
         finally {
@@ -85,6 +124,35 @@ public class ClientController {
         }
 
 
+    }
+
+    public void updateDropDown() throws FileNotFoundException {
+        File file = new File(JUST_ITEMS);
+        Scanner sc = new Scanner(file);
+
+        dropDown.getItems().clear();
+        while(sc.hasNextLine()){
+            String s = sc.nextLine();
+            dropDown.getItems().add(s);
+        }
+
+
+    }
+
+    public String findDescription(String item) throws FileNotFoundException {
+        File file = new File(DATABASE);
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNextLine()){
+            String s = sc.nextLine();
+            if(s.equals(item)){
+                s = sc.nextLine();
+                return s.substring(13);
+
+            }
+
+        }
+        return null;
     }
 
 }
---
+++ src/assignment7/Item.java
@@ -0,0 +1,79 @@
+package assignment7;
+
+
+import java.io.Serializable;
+
+public class Item implements Serializable {
+    private String itemName;
+    private String itemDescription;
+    private Double itemPrice = 1.00;
+    private Boolean sold;
+    private String username;
+    private static final long serialVersionUID = 1234L;
+
+    Item(String name, String description, Double price){//constructed only from server side
+        setItemName(name);
+        setItemDescription(description);
+        setItemPrice(price);
+        sold = false;
+    }
+
+    Item(String name, String description, Double price, String client){ //constructed only from client side
+        setItemName(name);
+        setItemDescription(description);
+        setItemPrice(price);
+        Sold();
+        setUsername(client);
+    }
+
+    private void setItemName(String name){
+        itemName = name;
+    }
+
+    private void setItemDescription(String description){
+        itemDescription = description;
+    }
+
+    void setItemPrice(Double price){
+        itemPrice = price;
+    }
+
+    void setUsername(String client){
+        username = client;
+    }
+
+    String getItemName(){
+        return itemName;
+    }
+
+    String getItemDescription(){
+        return itemDescription;
+    }
+
+    Double getItemPrice(){
+        return itemPrice;
+    }
+
+    String getUsername(){
+        return username;
+    }
+
+    boolean isSold(){
+        return sold;
+    }
+
+    public String toString(){
+        return null;
+
+    }
+
+    void Sold(){
+        sold = true;
+    }
+
+
+
+
+
+
+}
